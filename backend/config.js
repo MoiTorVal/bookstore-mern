b684cdc80f2729bef4ae0846f2f6b7fc7d2c0ef2
moises valdez <shmoi@moisess-MacBook-Pro.local>
2024-01-12 11:49:22 -0800

    add a database

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 3000;
+
+export const mongoDB =
+  "mongodb+srv://moisesvaldez075:ZfstDThQqmNY9vbU@books-store.w5gwoze.mongodb.net/?retryWrites=true&w=majority";
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,20 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDB } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.get("/", (req, res) => {
+  console.log(req);
+  res.status(200).send("Hello World!");
 });
+
+mongoose
+  .connect(mongoDB)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => err.message);
