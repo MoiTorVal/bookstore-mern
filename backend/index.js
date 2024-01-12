cd5f4ad96e1d8c45466dcdab837b7069255363b4
moises valdez <shmoi@moisess-MacBook-Pro.local>
2024-01-12 13:06:05 -0800

    create a new post function and send a post from postman

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,30 @@ import { Book } from "./models/bookModel.js";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!");
+});
+
+// Route for save a new Book
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.publishYear || !req.body.author) {
+      return res.status(400).send("Missing required fields");
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+    return res.status(201).send(book);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send(err.message);
+  }
 });
 
 mongoose
